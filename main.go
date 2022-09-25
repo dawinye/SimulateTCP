577eaaea9e8bc15629c55b0ca6697b47540ff47b
kkeith1018 <kwanke@bc.edu>
2022-09-25 15:50:18 -0400

    read message from command line and splits it to be used in function calls

--- main.go
+++ main.go
@@ -9,14 +9,25 @@ import (
 )
 
 // This one would call the tcpC file and run it
-func unicast_send(destination, message) {
+/*
+func unicast_send(destination string, message string) {
+	connection, err := net.Dial("tcp", destination)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 }
 
-func unicast_recieve(source, message) {
+func unicast_recieve(source string, message string) {
+	listener, err := net.Listen("tcp", source)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 }
-
+*/
 func main() {
 	//we kept this if we need to run commands in terminal from within main
 	cmnd := exec.Command("main.exe", "arg")
@@ -37,5 +48,26 @@ func main() {
 		fmt.Println(id[0])
 		//fmt.Println(scanner.Text())
 	}
+	//this is for reading user input sourced from linode tutorial
+	//format of the user input would be "send 2 message", in this scenario process 2 would be sent a message
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(">> ")
+		text, _ := reader.ReadString('\n')
 
+		fmt.Println(text)
+
+		splitted := strings.Split(text, " ")
+		process_destination := splitted[1]
+		message := splitted[2:]
+		fmt.Println(process_destination)
+		fmt.Println(message)
+
+		// message, _ := bufio.NewReader(c).ReadString('\n')
+		// fmt.Print("->: " + message)
+		// if strings.TrimSpace(string(text)) == "STOP" {
+		// 	fmt.Println("TCP client exiting...")
+		// 	return
+		// }
+	}
 }
